432b90de7f0b9cbc0599451b3555ec600259cad9
bloding <alaouiihouda@gmail.com>
2025-11-09 02:21:00 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
     outDir: 'dist',
   },
+  publicDir: 'src/public',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
